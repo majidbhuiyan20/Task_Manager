49eed4ab3e30fec9ea983da56e141f903422bb3f
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-09-10 03:55:45 +0600

    make login page

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/widgets/Screen_Background.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,12 +9,75 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        centerTitle: true,
+      body: ScreenBackground(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 80,),
+                Text("Get Started With", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                SizedBox(height: 24,),
+            
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                  )
+                ),
+                SizedBox(height: 8,),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                  ),
+                ),
+                SizedBox(height: 30,),
+                FilledButton(
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    fixedSize: Size.fromWidth(double.maxFinite),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () {},
+                  child: Icon(Icons.arrow_circle_right_outlined),
+                ),
+                SizedBox(height: 30,),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(onPressed: () {}, child: Text("Forgot Password?")),
+                      RichText(
+                        text: TextSpan(
+                          text: "Don't have an account? ",
+                          style: TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: "Sign Up",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
