ffe5eb5ec05513fec6d9dedd7710cbd365c421f3
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-09-12 03:54:10 +0600

    progress task screen update

---
+++ lib/ui/screens/progress_task_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/task_cout_by_status_card.dart';
+
+class ProgressTaskScreen extends StatefulWidget {
+  const ProgressTaskScreen({super.key});
+
+  @override
+  State<ProgressTaskScreen> createState() => _ProgressTaskScreenState();
+}
+
+class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              child: ListView.separated(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) =>
+                    TaskCountByStatusCard(
+                        title: "New",
+                        count: 10
+                    ),
+                separatorBuilder: (context, index) =>SizedBox(width: 8,),
+
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () {},
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+    );
+  }
+}
+
