37f1fe42bc87f7d1d36e3367a16972de0e061173
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-09-19 06:44:41 +0600

    make add screen ui

---
+++ lib/ui/screens/add_new_task_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/widgets/Screen_Background.dart';
+import 'package:task_manager/ui/widgets/tm_app_bar.dart';
+
+class AddNewTaskScreen extends StatefulWidget {
+  const AddNewTaskScreen({super.key});
+
+  @override
+  State<AddNewTaskScreen> createState() => _AddNewTaskScreenState();
+}
+
+class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TMAppBar(),
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Padding(padding: EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 32,),
+                  Text("Add new task", style: Theme.of(context).textTheme.titleLarge,),
+                  SizedBox(height: 8,),
+                  TextFormField(
+                    controller: _titleController,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      hintText: "Title",
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  TextFormField(
+                    controller: _descriptionController,
+                    maxLines: 6,
+                    decoration: InputDecoration(
+                      hintText: "Description",
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  FilledButton(onPressed: (){}, child: Text("Add")),
+                ],
+            ),
+          ),
+          ),
+        
+        ),
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+}
